dfcd312f7e5fa310ae712253081193f43bef48de
mokrooussovi <ivan.mokrooussov@dcmail.ca>
2022-02-05 19:54:39 -0500

    creating live site for Group3Lab1

---
+++ README.md
@@ -0,0 +1,3 @@
+# group3lab1
+Group 3's Lab 1 assignment for WEBD6201
+
--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,21 @@
 // IIFE -- Immediately Invoked Function Expression
 // AKA -- Anonymous Self-Executing Function
+
+
 (function()
 {
+    
+    // import { ReplaceLink } from './functions.js';
     function DisplayHomePage()
     {
+        // let DocumentBody = document.body;
+
+        // Variable for text content to be displayed on page
+        let paragraph1 = [`Welcome to our site!\n This webpage was created by Ivan Mokrooussov and\n
+                    Will Estanislao for the WEBD6201 Client-Side Scripting course\.\n
+                    This website demonstrates the use of DOM Manipulation for Lab 1\.\n
+                    Have fun exploring\!\n`];
+
         console.log("Home Page");
         let AboutUsButton = document.getElementById("AboutUsButton");
         AboutUsButton.addEventListener("click", function()
@@ -11,47 +23,56 @@
             location.href = "about.html";
         });
 
-        // Step 1. get an entry point(s) (insertion point / deletion point) reference 
-        let DocumentBody = document.body;
-        let MainContent = document.getElementsByTagName("main")[0];
+        // TO-DO: adjust to insert into div w/in main instead of outside (maybe...)
+        // Target the main element to start inserting new content in
+        let MainContent = document.getElementsByClassName('container')[0];
+        let ImgContent = document.getElementsByClassName('InnerBodyDiv')[0];
 
-        // Step 2. create an element(s) to insert
-        let MainParagraph = document.createElement("p");
-        let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class ="mt-3">This is the Article Paragraph</p>`
-
-        // Step 3. configure the new element
-        MainParagraph.setAttribute("id", "MainParagraph");
-        MainParagraph.setAttribute("class", "mt-3");
-        MainParagraph.textContent = "This is the Main Paragraph";
-        Article.setAttribute("class", "container");
-
-        // Step 4. Add / Insert the new element
-        MainContent.appendChild(MainParagraph);
-        Article.innerHTML = ArticleParagraph;
-        DocumentBody.appendChild(Article);
-
-        // to insert before
-        // target a location (element) to insert before 
-        // TargetElement.before(elementToInsert);
-
-        // to delete
-        //Article.remove();
+        // Create a new paragraph element to insert text in
+        let ParaOne = document.createElement("p");
+        ParaOne.setAttribute("id", "ParaOne");
+        ParaOne.setAttribute("class", "mt-3");
+        // Build the paragraph by adding the string array together
+        ParaOne.textContent = paragraph1;
+        ParaOne.innerHTML += ParaOne.textContent;
+        console.log(MainContent);
+        console.log(ParaOne);
+        
+        // Insert new elements onto document
+        MainContent.insertBefore(ParaOne, ImgContent);
     }
 
-    function DisplayProductsPage()
+    function DisplayProjectsPage()
     {
-        console.log("Products Page");
+        console.log("Projects Page");
+
+        ProjectsContent();
+ 
     }
 
     function DisplayServicesPage()
     {
         console.log("Services Page");
+
+        ServiceContent();
+
     }
 
     function DisplayAboutPage()
     {
         console.log("About Page");
+
+        AboutContent();
+        
+    }
+
+    function DisplayRecourcesPage()
+    {
+        console.log("Human Resources Page");
+
+        //let DocumentBody = document.body.style.backgroundImage;
+        //document.body.style.backgroundImage = "url(view2.png)";
+
     }
 
     function DisplayContactPage()
@@ -80,6 +101,7 @@
 
     function DisplayContactListPage()
     {
+
         if(localStorage.length > 0)
         {
             let contactList = document.getElementById("contactList");
@@ -120,13 +142,19 @@
     {
         console.log("App Started!");
 
+        // Replace an option in the navbar
+        ReplaceLink();
+
+        // Add in new human resources link
+        AddNavLink();
+
         switch(document.title)
         {
             case "Home":
                 DisplayHomePage();
                 break;
-            case "Our Products":
-                DisplayProductsPage();
+            case "Our Favourite Projects":
+                DisplayProjectsPage();
                 break;
             case "Our Services":
                 DisplayServicesPage();
@@ -134,15 +162,147 @@
             case "About Us":
                 DisplayAboutPage();
                 break;
+            case "Human Resources":
+                DisplayRecourcesPage();
+                break;
             case "Contact Us":
                 DisplayContactPage();
                 break;
             case "Contact-List":
                 DisplayContactListPage();
                 break;
-
         }
        
+    }
+
+    // Function to replace text in nav bar
+    function ReplaceLink()
+    {
+        // Select the specific navigation element to be changed - products link
+        let NavContents = document.body.getElementsByClassName("nav-item")[1];
+
+        // Create the new navigation list link and insert it
+        // Completely replaces what was hardcoded in html
+        let EditNavOption = '<a class="nav-link" href="products.html"><i class="fas fa-project-diagram"></i> Projects</a>';
+        NavContents.innerHTML = EditNavOption;
+
+    }
+
+    function AddNavLink()
+    {
+        // Select the specific navigation element to be changed - products link
+        let NavContents = document.body.getElementsByClassName("nav-item")[3];
+        let NavList = document.createElement("li");
+        NavList.setAttribute("class", "nav-item");
+
+        let NewNavOption = '<a class="nav-link" href="resources.html"><i class="far fa-id-card"></i> Human Resources</a>';
+        NavList.innerHTML = NewNavOption;
+        NavContents.after(NavList);
+        
+        // For testing - Check if html element is inserted
+        // console.log(NavList);
+        
+    }
+
+    function ProjectsContent()
+    {
+        let paragraph1 = ['Creating File Text Editor - Visual Studio 2019.', 'FormAwesomness - A simple application with disappear button', 
+                            'EntityFramework - Retriving data from local database, migration and following convertion this data into entities objects'];
+        let paragraph2 = ['Draw A Square - An application that lets you draw squares', 
+                        'Numbers Guessing Game - A simple guessing game coded in C#', 
+                        'Quiz Game - A quiz game programmed in Turing. It had approximately 10 random trivia questions and music\!\nUnfortunately this game has been lost to time.'];
+
+        // Select an element to find a position in the document
+        let div1 = document.body.getElementsByClassName('list')[0];
+        let div2 = document.body.getElementsByTagName('h3')[1];
+        
+        // Create the ul that will contain the first set of projects listed
+        let ProjectSet1 = document.createElement("ul");
+        ProjectSet1.setAttribute("id", "ListOne");
+        ProjectSet1.setAttribute("class", "list-1");
+
+        // Create ul that will contain second set of projects
+        let ProjectSet2 = document.createElement("ul");
+        ProjectSet2.setAttribute("id", "ListTwo");
+        ProjectSet2.setAttribute("class", "list-1");
+        
+        // Loop to set Text
+        list = SetTextLoop(paragraph1);
+        list2 = SetTextLoop(paragraph2);
+
+        ProjectSet1.innerHTML = list;
+        ProjectSet2.innerHTML = list2;
+        div1.appendChild(ProjectSet1);
+        div2.after(ProjectSet2);
+
+        console.log(ProjectSet1);
+    }
+
+    function ServiceContent()
+    {
+        let ContentBody = document.body.getElementsByClassName('photos')[0];
+        let paragraph1 = ['Expertise in Web Design Techniques', 'Proficiency In Programming Languages', 
+                        'Graphic Design and Photography'];
+
+        // Create the element
+        let ProjectSet1 = document.createElement("ul");
+        ProjectSet1.setAttribute("id", "ListThree");
+        ProjectSet1.setAttribute("class", "list-1");
+
+        list = SetTextLoop(paragraph1);
+
+        ProjectSet1.innerHTML = list;
+        ContentBody.insertAdjacentElement("beforebegin", ProjectSet1);
+
+    }
+
+    function AboutContent()
+    {
+        // Find element opening to start inserting from - Student id and name
+        let ContentBody = document.body.getElementsByTagName('img')[0];
+        let ContentBody2 = document.body.getElementsByTagName('img')[1];
+        let student1 = ['Ivan Mokrooussov', 'StudentID: 100808605'];
+        let student2 = ['Will Estanislao', 'StudentID: 100786862']
+
+        // About content to be inserted
+        let paragraph1 = '[Enter some info here]';
+        let paragraph2 = `Hello, I\'m William Estanislao! I\'m currently in the 2 year Computer Programming
+                        course in Durham College. I specialize primarily in developing applications and working
+                        with programming languages such as C# and C++. I'm currently working on creating some games
+                        using Unity and Visual Studio.`;
+        // Create the element
+        let ProjectSet1 = document.createElement("ul");
+        ProjectSet1.setAttribute("id", "student-1");
+        ProjectSet1.setAttribute("class", "list-1");
+        // Create ul that will contain second set of projects
+        let ProjectSet2 = document.createElement("ul");
+        ProjectSet2.setAttribute("id", "student-2");
+        ProjectSet2.setAttribute("class", "list-1");
+
+        let textContent1 = document.createElement('p');
+        textContent1.textContent = paragraph2;
+
+        list = SetTextLoop(student1);
+        list2 = SetTextLoop(student2);
+
+        ProjectSet1.innerHTML = list;
+        ProjectSet2.innerHTML = list2;
+        ContentBody.insertAdjacentElement("beforebegin", ProjectSet1);
+        ContentBody2.insertAdjacentElement("beforebegin",ProjectSet2);
+        list = "";
+    }
+
+
+    function SetTextLoop(textArray)
+    {
+        let list = '';
+
+        for (const textLine of textArray)
+        {
+            list += `<li>${textLine}</li>`;
+        }
+
+        return list;
     }
 
     window.addEventListener("load", Start);
